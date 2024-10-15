7dccecfa5b55de7f4e924ddd18ff1fa40e3c2ffe
abdullahozer11 <abdullahozer11@hotmail.com>
2024-10-15 21:08:13 +0200

    Remove flag button to have uni language per domain.

--- src/script.js
+++ src/script.js
@@ -1,24 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const languageToggle = document.getElementById('languageToggle');
     const soundToggle = document.getElementById('soundToggle');
     const video = document.getElementById('invitationVideo');
-    const languageIcon = document.getElementById('languageIcon');
-    let isEnglish = true;
-
-    languageToggle.addEventListener('click', function() {
-        isEnglish = !isEnglish;
-        if (isEnglish) {
-            video.src = 'english_invitation.mp4';
-            languageIcon.src = 'tr.png';
-            languageIcon.alt = 'Turkish';
-        } else {
-            video.src = 'turkish_invitation.mp4';
-            languageIcon.src = 'gb.png';
-            languageIcon.alt = 'English';
-        }
-        video.load();
-        video.play();
-    });
 
     soundToggle.addEventListener('click', function() {
         if (video.muted) {
