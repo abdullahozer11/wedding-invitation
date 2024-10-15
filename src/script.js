8bdd9c00da4db04ca3594d136aea07652c94ebdd
abdullahozer11 <abdullahozer11@hotmail.com>
2024-10-15 19:42:24 +0200

    First commit

---
+++ src/script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const languageToggle = document.getElementById('languageToggle');
+    const soundToggle = document.getElementById('soundToggle');
+    const video = document.getElementById('invitationVideo');
+    const languageIcon = document.getElementById('languageIcon');
+    let isEnglish = false;
+
+    languageToggle.addEventListener('click', function() {
+        isEnglish = !isEnglish;
+        if (isEnglish) {
+            video.src = 'english_invitation.mp4';
+            languageIcon.src = 'images/tr.png';
+            languageIcon.alt = 'Turkish';
+        } else {
+            video.src = 'turkish_invitation.mp4';
+            languageIcon.src = 'gb.png';
+            languageIcon.alt = 'English';
+        }
+        video.load();
+        video.play();
+    });
+
+    soundToggle.addEventListener('click', function() {
+        if (video.muted) {
+            video.muted = false;
+            soundToggle.innerHTML = '<i class="fas fa-volume-up"></i>';
+        } else {
+            video.muted = true;
+            soundToggle.innerHTML = '<i class="fas fa-volume-mute"></i>';
+        }
+    });
+});
